f66fd6339efe3183c649f1b0b79a29d7f9eecb3b
Michal Asgraf Turek <asgraf@gmail.com>
2014-09-16 08:20:20 +0200

    search field whitelisting

--- Controller/Component/SearchComponent.php
+++ Controller/Component/SearchComponent.php
@@ -12,11 +12,12 @@ class SearchComponent extends Component {
 	
 	public function initialize(Controller $controller) {
 		$this->controller = $controller;
+
 	}
 
-	public function getSearchConditions() {
-		$conditions = array();
+	public function getSearchConditions($universalSearchFieldsWhitelist=array(),$advancedSearchFieldsWhitelist=array()) {
 		$modelClass = $this->controller->modelClass;
+		$conditions = array();
 		$plugin = $this->controller->plugin?$this->controller->plugin.'.':null;
 		if(empty($this->controller->uses) || !in_array($plugin.$modelClass,$this->controller->uses)) return array();
 		/**
@@ -25,8 +26,14 @@ class SearchComponent extends Component {
 		$Model = $this->controller->$modelClass;
 		$schema = $Model->schema();
 		$q = $this->controller->request->param('q')?:$this->controller->request->query('q');
-		foreach(array_keys($Model->validate) as $fieldname) {
-			if(in_array($fieldname,array('id','password'))) continue;
+		$searchFieldsWhitelist = ($q!==null && $q!=='')?$universalSearchFieldsWhitelist:$advancedSearchFieldsWhitelist;
+		if(empty($searchFieldsWhitelist) || in_array($modelClass,$searchFieldsWhitelist)) {
+			$searchFields = array_keys($Model->validate);
+		} else {
+			$searchFields = Hash::get($searchFieldsWhitelist,$modelClass)?:array();
+		}
+		foreach($searchFields as $fieldname) {
+			if((!empty($searchFieldsWhitelist) || in_array($modelClass,$searchFieldsWhitelist)) && in_array($fieldname,array('id','password'))) continue;
 			$conditionField = $modelClass.'.'.$fieldname;
 			$fieldValue = $this->controller->request->param($fieldname)?:$this->controller->request->query($fieldname);
 			if($fieldValue!==null && $fieldValue!=='') {
@@ -49,9 +56,15 @@ class SearchComponent extends Component {
 		}
 		foreach(array('belongsTo','hasOne') as $relation) {
 			foreach($Model->$relation as $alias=>$assocData) {
+				if(empty($searchFieldsWhitelist) || in_array($alias,$searchFieldsWhitelist)) {
+					$searchFields = array_keys($Model->$alias->validate);
+				} else {
+					$searchFields = Hash::get($searchFieldsWhitelist,$alias);
+				}
+				if(empty($searchFields)) continue;
 				$assocSchema = $Model->$alias->schema();
-				foreach(array_keys($Model->$alias->validate) as $fieldname) {
-					if(in_array($fieldname,array('id','password'))) continue;
+				foreach($searchFields as $fieldname) {
+					if((!empty($searchFieldsWhitelist) || in_array($alias,$searchFieldsWhitelist)) && in_array($fieldname,array('id','password'))) continue;
 					$conditionField = $alias.'.'.$fieldname;
 					$fieldValue = $this->controller->request->param($conditionField)?:$this->controller->request->query($conditionField);
 					if($fieldValue!==null && $fieldValue!=='') {
@@ -77,39 +90,108 @@ class SearchComponent extends Component {
 		$ids = array();
 		foreach($Model->hasMany as $alias=>$assocData) {
 			$fieldValue = $this->controller->request->param($alias)?:$this->controller->request->query($alias);
-			if($fieldValue!==null && $fieldValue!=='') {
-				$ids[]=$Model->$alias->find('list',array('fields'=>$alias.'.'.$assocData['foreignKey'],'conditions'=>array_merge(array($alias.'.'.$Model->$alias->primaryKey=>$fieldValue), $assocData['conditions']?:array())));
+			if($fieldValue!==null && $fieldValue!=='' && !is_array($fieldValue)) {
+				if(!(!empty($searchFieldsWhitelist) && !in_array($alias,$searchFieldsWhitelist) && !in_array($Model->$alias->primaryKey,Hash::get($searchFieldsWhitelist,$alias)?:array()))) {
+					$ids[]=$Model->$alias->find('list',array('fields'=>$alias.'.'.$assocData['foreignKey'],'conditions'=>array_merge(array($alias.'.'.$Model->$alias->primaryKey=>$fieldValue), $assocData['conditions']?:array())));
+				}
 			}
-			if($q!==null && $q!=='' && $assocData['foreignKey']!='parent_id') {
+			if(empty($searchFieldsWhitelist) && $q!==null && $q!=='' && $assocData['foreignKey']!='parent_id') {
 				$ids[]=$Model->$alias->find('list',array('fields'=>$alias.'.'.$assocData['foreignKey'],'conditions'=>array_merge(array($alias.'.'.$Model->$alias->displayField.' LIKE'=>'%'.$q.'%'), $assocData['conditions']?:array())));
+			}
+			if(in_array($alias,$searchFieldsWhitelist)) {
+				$searchFields = array_keys($Model->$alias->validate);
+			} else {
+				$searchFields = Hash::get($searchFieldsWhitelist,$alias);
+			}
+			if(!empty($searchFieldsWhitelist) && !empty($searchFields)) {
+				$assocSchema = $Model->$alias->schema();
+				$assocConds = array();
+				foreach($searchFields as $fieldname) {
+					$conditionField = $alias.'.'.$fieldname;
+					$fieldValue = $this->controller->request->param($conditionField)?:$this->controller->request->query($conditionField);
+					if($fieldValue!==null && $fieldValue!=='') {
+						if(is_array($fieldValue)) {
+							$assocConds[]['OR'][$conditionField]=$fieldValue;
+						} elseif(in_array($assocSchema[$fieldname]['type'],array('string','text'))) {
+							$assocConds[$conditionField.' LIKE']='%'.$fieldValue.'%';
+						} else {
+							$assocConds[$conditionField]=$fieldValue;
+						}
+						continue;
+					}
+					if($q!==null && $q!=='' && $assocData['foreignKey']!='parent_id') {
+						if(in_array($assocSchema[$fieldname]['type'],array('string','text','date','time','datetime'))) {
+							$assocConds['OR'][$conditionField.' LIKE']='%'.$q.'%';
+						} elseif($q>0) {
+							$assocConds['OR'][$conditionField]=$q;
+						}
+					}
+				}
+				if(!empty($assocConds)) {
+					$ids[]=$Model->$alias->find('list',array('fields'=>$alias.'.'.$assocData['foreignKey'],'conditions'=>$assocConds));
+				}
 			}
 		}
 
 		foreach($Model->hasAndBelongsToMany as $alias=>$assocData) {
 			$fieldValue = $this->controller->request->param($alias)?:$this->controller->request->query($alias);
-			if($fieldValue!==null && $fieldValue!=='') {
-                list($widthplugin,$widthmodelname) = pluginSplit($assocData['with']);
-                if(empty($Model->$widthmodelname)) throw new InternalErrorException(__('%s model has invalid relation with %s model',$Model->alias,$assocData['with']));
-				$ids[]=$Model->$widthmodelname->find('list',array('fields'=>$widthmodelname.'.'.$assocData['foreignKey'],'conditions'=>array_merge(array($widthmodelname.'.'.$assocData['associationForeignKey']=>$fieldValue), $assocData['conditions']?:array())));
+			if($fieldValue!==null && $fieldValue!=='' && !is_array($fieldValue)) {
+				if(!(!empty($searchFieldsWhitelist) && !in_array($alias,$searchFieldsWhitelist) && !in_array($Model->$alias->primaryKey,Hash::get($searchFieldsWhitelist,$alias)?:array()))) {
+					list($widthplugin,$widthmodelname) = pluginSplit($assocData['with']);
+					if(empty($Model->$widthmodelname)) throw new InternalErrorException(__('%s model has invalid relation with %s model',$Model->alias,$assocData['with']));
+					$ids[]=$Model->$widthmodelname->find('list',array('fields'=>$widthmodelname.'.'.$assocData['foreignKey'],'conditions'=>array_merge(array($widthmodelname.'.'.$assocData['associationForeignKey']=>$fieldValue), $assocData['conditions']?:array())));
+				}
 			}
-			if($q!==null && $q!=='') {
+			if(in_array($alias,$searchFieldsWhitelist)) {
+				$searchFields = array_keys($Model->$alias->validate);
+			} else {
+				$searchFields = Hash::get($searchFieldsWhitelist,$alias);
+			}
+			if(!empty($searchFieldsWhitelist) && !empty($searchFields)) {
 				list($widthplugin,$widthmodelname) = pluginSplit($assocData['with']);
 				if(empty($Model->$widthmodelname)) throw new InternalErrorException(__('%s model has invalid relation with %s model',$Model->alias,$assocData['with']));
-				$ids[]=$Model->$widthmodelname->find('list',array('fields'=>$widthmodelname.'.'.$assocData['foreignKey'],'conditions'=>array_merge(array($widthmodelname.'.'.$Model->$widthmodelname->displayField=>$fieldValue), $assocData['conditions']?:array())));
+				$assocSchema = $Model->$alias->schema();
+				$assocConds = array();
+				foreach($searchFields as $fieldname) {
+					$conditionField = $alias.'.'.$fieldname;
+					$fieldValue = $this->controller->request->param($conditionField)?:$this->controller->request->query($conditionField);
+					if(empty($fieldValue) && $fieldname==$Model->$alias->displayField) $fieldValue =$this->controller->request->param($alias)?:$this->controller->request->query($alias);
+					if($fieldValue!==null && $fieldValue!=='') {
+						if(is_array($fieldValue)) {
+							$assocConds[]['OR'][$conditionField]=$fieldValue;
+						} elseif(in_array($assocSchema[$fieldname]['type'],array('string','text'))) {
+							$assocConds[$conditionField.' LIKE']='%'.$fieldValue.'%';
+						} else {
+							$assocConds[$conditionField]=$fieldValue;
+						}
+						continue;
+					}
+					if($q!==null && $q!=='' && $assocData['foreignKey']!='parent_id') {
+						if(in_array($assocSchema[$fieldname]['type'],array('string','text','date','time','datetime'))) {
+							$assocConds['OR'][$conditionField.' LIKE']='%'.$q.'%';
+						} elseif($q>0) {
+							$assocConds['OR'][$conditionField]=$q;
+						}
+					}
+				}
+				if(!empty($assocConds)) {
+					$habm_ids=$Model->$alias->find('list',array('fields'=>$alias.'.'.$Model->$alias->primaryKey,'conditions'=>$assocConds));
+					$ids[]=$Model->$widthmodelname->find('list',array('fields'=>$widthmodelname.'.'.$assocData['foreignKey'],'conditions'=>array_merge(array($widthmodelname.'.'.$assocData['associationForeignKey']=>$habm_ids), $assocData['conditions']?:array())));
+				}
 			}
-
 		}
+
 		$conds = array();
 		switch(count($ids)) {
 			case 0:
 			break;
 		
 			case 1:
-				$conds[$modelClass.'.'.$Model->primaryKey]=$ids[0];
+				$conds[$modelClass.'.'.$Model->primaryKey]=array_unique($ids[0]);
 			break;
 			
 			default:
-				$conds[$modelClass.'.'.$Model->primaryKey]=call_user_func_array($this->controller->request->query('q')?'array_merge':'array_intersect',$ids);
+				$conds[$modelClass.'.'.$Model->primaryKey]=array_unique(call_user_func_array($this->controller->request->query('q')?'array_merge':'array_intersect',$ids));
 			break;
 		}
 		if(!empty($conds)) {

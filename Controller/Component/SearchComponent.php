Asgraf/AsgrafSearch

commit 42dd2c48fdbfe704ab514bb649aa5502a0243063

fix for virtualFields on belongsTo relation

Controller/Component/SearchComponent.php
@@ -81,7 +81,9 @@ class SearchComponent extends Component {
 						continue;
 					}
 					if($q!==null && $q!=='' && $assocData['foreignKey']!='parent_id') {
-						if(in_array($assocSchema[$fieldname]['type'],array('string','text','date','time','datetime'))) {
+						if(array_key_exists($fieldname,$Model->$alias->virtualFields)) {
+							$conditions['OR'][$Model->$alias->virtualFields[$fieldname].' LIKE']='%'.$q.'%';
+						} elseif(!in_array($assocSchema[$fieldname]['type'],array('string','text','date','time','datetime'))) {
 							$conditions['OR'][$conditionField.' LIKE']='%'.$q.'%';
 						} elseif($q>0) {
 							$conditions['OR'][$conditionField]=$q;

commit 2254643e586805a4f22c0867342f3a49deba941b

fixed typo introduced in 42dd2c48fdbfe704ab514bb649aa5502a0243063

Controller/Component/SearchComponent.php
@@ -83,7 +83,7 @@ class SearchComponent extends Component {
 					if($q!==null && $q!=='' && $assocData['foreignKey']!='parent_id') {
 						if(array_key_exists($fieldname,$Model->$alias->virtualFields)) {
 							$conditions['OR'][$Model->$alias->virtualFields[$fieldname].' LIKE']='%'.$q.'%';
-						} elseif(!in_array($assocSchema[$fieldname]['type'],array('string','text','date','time','datetime'))) {
+						} elseif(in_array($assocSchema[$fieldname]['type'],array('string','text','date','time','datetime'))) {
 							$conditions['OR'][$conditionField.' LIKE']='%'.$q.'%';
 						} elseif($q>0) {
 							$conditions['OR'][$conditionField]=$q;